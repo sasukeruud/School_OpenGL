5c97313c414f310823070bb511d0ee9ee30c9fbd
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-09-27 17:59:21 +0200

    added 2D triangle and square

---
+++ Engine/GeometricTools/GeometricTools.h
@@ -0,0 +1,4 @@
+namespace GeometricTools{
+  constexpr float UnitTriangle2D[3*2] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f}; // [2,3]
+  constexpr float UnitSquare2D[6*2] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f};
+}
