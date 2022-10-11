eb800fc0f0cd0935990f5d5c6c17a94b15108921
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-10-11 12:46:55 +0200

    added color arrays

--- Engine/GeometricTools/GeometricTools.h
+++ Engine/GeometricTools/GeometricTools.h
@@ -1,4 +1,6 @@
 namespace GeometricTools{
   constexpr float UnitTriangle2D[3*2] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f}; // [2,3]
   constexpr float UnitSquare2D[6*2] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f};
+  constexpr float ColorTriangle2D[3*3] = {0.0f, 0.0f, 0.0f,	0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+  constexpr float ColorSquare2D[3*4] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 }
