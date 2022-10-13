3a91ad3366065ce245349ce17aba145d1e58aee9
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-10-13 23:01:22 +0200

    added comments, change constructor

--- Engine/VertexBuffer/VertexBuffer.cpp
+++ Engine/VertexBuffer/VertexBuffer.cpp
@@ -4,29 +4,33 @@
 //Engine libraries
 #include "VertexBuffer.h"
 
-VertexBuffer::VertexBuffer(const void* verticies, GLsizei size) {
+VertexBuffer::VertexBuffer(GLsizei size) {
+	glGenBuffers(size, &VertexBufferID);
+}
+
+VertexBuffer::VertexBuffer(const void* vertices,GLsizei size) {
 	glGenBuffers(size, &VertexBufferID);
 }
 
 VertexBuffer::~VertexBuffer() {}
 
-void VertexBuffer::SetData(const void* verticies){
+void VertexBuffer::SetData(const void* verticies, GLenum target){
 	int32_t bufferData = 0;
 
 	glGenBuffers(1, &VertexBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
+	glBindBuffer(target, VertexBufferID);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GeometricTools::UnitTriangle2D), verticies, GL_STATIC_DRAW);
-	glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_SIZE, &bufferData);
+	glBufferData(target, sizeof(GeometricTools::UnitTriangle2D), verticies, GL_STATIC_DRAW);
+	glGetBufferParameteriv(target, GL_BUFFER_SIZE, &bufferData);
 
 	if (bufferData == 0) std::cout << "ERROR:: NO DATA IN BUFFER" << std::endl;
 }
 
-void VertexBuffer::Bind(GLuint index, GLint valuePoint) const {
+void VertexBuffer::Bind(GLuint index, GLint size) const {
 	glEnableVertexAttribArray(index);
 	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
 	glVertexAttribPointer(	index,
-							valuePoint,
+							size,
 							GL_FLOAT,
 							GL_FALSE,
 							sizeof(float) * 3,
--- Engine/VertexBuffer/VertexBuffer.h
+++ Engine/VertexBuffer/VertexBuffer.h
@@ -15,17 +15,42 @@ private:
 	GLuint VertexBufferID;
 
 public:
-	// Constructor. It initializes with a data buffer and the size of it.
-	// Note that the buffer will be bound on construction.
+	/*
+	Default constructor. just a placeholder
+	*/
 	VertexBuffer() = default;
+
+	/*
+	Constructor. It initializes with a data buffer and the size of it.
+	Generets the VBO
+	size		: How many buffers to be created
+	*/
+	VertexBuffer(GLsizei size);
+
+	/*
+	Constructor. It initializes with a data buffer and the size of it.
+	Generets the VBO
+	verticies	: Data that will be copied onto the buffer
+	size		: How many buffers to be created
+	*/
 	VertexBuffer(const void* vertices, GLsizei size);
+
+	//Deconstructor
 	~VertexBuffer();
 	
-	// Sets data for the vbo
-	void SetData(const void* verticies);
+	/*
+	Sets data for the vbo
+	verticies	: Data that will be copied onto the buffer
+	target		: Spesify what type of buffer object to use. Normaly GL_ARRAY_BUFFER or GL_ELEMENT_ARRAY_BUFFER
+	*/
+	void SetData(const void* verticies, GLenum target);
 
-	//Binds vbo to vao
-	void Bind(GLuint index, GLint valuePoint) const;
+	/*
+	Binds vbo to vao
+	index	: index number of vertex attribute
+	size	: How many components per vertex
+	*/
+	void Bind(GLuint index, GLint size) const;
 
 	// Unbind what is bound to the buffer, if no vbo is bound it will be set to 0
 	void Unbind() const;
