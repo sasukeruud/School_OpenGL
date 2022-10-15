ff0ee638e7605654ff5a0e076951af2af721a944
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-10-15 11:06:59 +0200

    made SetData to a template function

--- Engine/VertexBuffer/VertexBuffer.cpp
+++ Engine/VertexBuffer/VertexBuffer.cpp
@@ -14,18 +14,6 @@ VertexBuffer::VertexBuffer(const void* vertices,GLsizei size) {
 
 VertexBuffer::~VertexBuffer() {}
 
-void VertexBuffer::SetData(const void* verticies, GLsizeiptr size) {
-	int32_t bufferData = 0;
-
-	glGenBuffers(1, &VertexBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
-
-	glBufferData(GL_ARRAY_BUFFER, size, verticies, GL_STATIC_DRAW);
-	glGetBufferParameteriv(GL_ARRAY_BUFFER, GL_BUFFER_SIZE, &bufferData);
-
-	if (bufferData == 0) std::cout << "ERROR:: NO DATA IN BUFFER" << std::endl;
-}
-
 void VertexBuffer::Bind() const {
 	glBindBuffer(GL_ARRAY_BUFFER, VertexBufferID);
 }
