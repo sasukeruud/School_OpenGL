a9c35b664747c12eaa6827d3b73b64b7b794ac4d
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-10-25 22:03:02 +0200

    added function for grid, only pair not odd

--- Engine/GeometricTools/GeometricTools.h
+++ Engine/GeometricTools/GeometricTools.h
@@ -27,111 +27,64 @@ namespace GeometricTools{
                                             0.0f, 0.0f, 0.0f, 
                                             0.0f, 0.0f, 0.0f};
 
-    //std::vector<glm::vec3> vertices;      || std::vector<glm::uvec3> indices;
-
-    /*
-  * Template function to generate a grid in 2D
-  * x is for how many across the width of the grid and y is for how many to create in the height.
-  * std::vector<glm::vec3>  ||  std::vector<glm::uvec3> are normaly used
-  */
-    template<class T> void UnitGridGeometry2D(int x, int y, T vertices){
-        for (int j = 0; j <= y; ++j)
+    template<class T, class U> void GenGrid(int N, T &verticies, U &indicies){
+        int half_N = N/2;
+        for (float j = -(N/2); j <= (float)N/2; j++)
         {
-            for (int i = 0; i <= x; ++i)
+            for (float i = -(N/2); i <= N/2; i++)
             {
-                GLfloat x_location = (float)i / (float)x;
-                GLfloat y_location = (float)j / (float)y;
-                GLfloat z_location = 0;
-
-                //New location added to the grid
-                //TODO: find out how this workes
-                vertices.push_back(glm::vec3(x_location * 2.0f - 1.0f, y_location * 2.0f - 1.0f, z_location));
-
-                std::cout << "x_location : " << x_location << std::endl;
-                std::cout << "y_location : " << y_location << std::endl;
-                std::cout << "z_location : " << z_location << std::endl;
-                std::cout << "_______________" << std::endl;
+                float x = (float)i/(float)N;
+                float y = (float)j/(float)N;
+                float z = sin(x*2.0f*3.141526f) * sin(y*2.0f*3.141526f) * 0.1f;
+                verticies.push_back(glm::vec3(x,y,z));
             }
-            std::cout << "#################" << std::endl;
+            
         }
 
-        //Creates two triangles to make up the square of the grid
-        /*
-        for (int j = 0; j < y; ++j) //Goes through all of in height
+        for (int j = 0; j < N; j++)
         {
-            for (int i = 0; i < x; ++i) //Goes through the width 
+            for (int i = 0; i < N; i++)
             {
-                int row1 = j * (x + 1);
-                int row2 = (j + 1) * (x + 1);
+                int row1 = j * (N+1);
+                int row2 = (j+1) * (N+1);
 
-                // triangle 1
-                indices.push_back(glm::uvec3(row1 + i, row1 + i + 1, row2 + i));
+                //Triangle 1
+                indicies.push_back(glm::uvec3(row1 + i,row1 + i + 1,row2 + i + 1));
 
-                // triangle 2
-                indices.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
-            }
-        }*/
-    };
-
-    //For making squares in the grid    :: GL_LINES
-    template<class T> void UnitGridTopologySquare(int x, int y, T indices){
-        for (int j = 0; j < y; ++j) //Goes through all of in height
-        {
-            std::cout << j << std::endl;
-            std::cout << "=================" << std::endl;
-            for (int i = 0; i < x; ++i) //Goes through the width 
-            {
-                int row1 = j * (x + 1);
-                int row2 = (j + 1) * (x + 1);
-
-                
-                
-
-                // triangle 1
-                indices.push_back(glm::uvec3(row1 + i, row1 + i + 1, row2 + i));
-                std::cout << "row1 + i : " << row1 + i << std::endl;
-                std::cout << "row1 + i + 1 : " << row1 + i + 1 << std::endl;
-                std::cout << "row2 + i : " << row2 + i << std::endl;
-                std::cout <<"_______________" << std::endl;
-
-                // triangle 2
-                indices.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
-                std::cout << "row1 + i : " << row1 + i << std::endl;
-                std::cout << "row2 + i + 1 : " << row2 + i + 1 << std::endl;
-                std::cout << "row2 + i : " << row2 + i << std::endl;
-                std::cout <<"_______________" << std::endl;
+                //Triangle 2
+                indicies.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
             }
             
         }
     }
 
-    //Function to make triangles in grid
-    template<class T> void UnitGridTopologyTriangles(int x, int y, T indices){
-        //Creates two triangles to make up the square of the grid
-        for (int j = 0; j < y; ++j) //Goes through all of in height
+    template<class T, class U> void GenGrid(int X, int Y, T &verticies, U &indices){
+        for (size_t i = 0; i < X/2; i++)
         {
-            for (int i = 0; i < x; ++i) //Goes through the width 
+            for (size_t j = 0; j < Y/2; j++)
             {
-                int row1 = j * (x + 1);
-                int row2 = (j + 1) * (x + 1);
-
-                // triangle 1
-                indices.push_back(glm::uvec3(row1 + i, row1 + i + 1, row2 + i));
-
-                //indices.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
-
-                // triangle 2
-                indices.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
+                float x_location = (float)i/(float)X;
+                float y_location = (float)j/(float)Y;
+                float z_location = sin(x*2.0f*3.141526f) * sin(y*2.0f*3.141526f) * 0.1f;
+                verticies.push_back(glm::vec3(x_location,y_location,z_location));
             }
+            
         }
-    }
+        
+        for (int j = 0; j < X; j++)
+        {
+            for (int i = 0; i < Y; i++)
+            {
+                int row1 = j * (X+1);
+                int row2 = (j+1) * (X+1);
 
-    template <class T> T UnitGridTopologyTriangles2(int x, int y){
-        float test[] = {(UnitTriangle2D[0]/x),(UnitTriangle2D[1]/y),
-                        (UnitTriangle2D[2]/x),(UnitTriangle2D[3]/y),
-                        (UnitTriangle2D[4]/x),(UnitTriangle2D[5]/y)};
+                //Triangle 1
+                indicies.push_back(glm::uvec3(row1 + i,row1 + i + 1,row2 + i + 1));
 
-        //std::cout << test[0] << std::endl;
-        return test; 
+                //Triangle 2
+                indicies.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
+            }
+            
+        }
     }
 }
