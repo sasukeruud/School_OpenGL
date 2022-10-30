85e74baf638d376d082648381211c1d1d6b0640d
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-10-30 14:01:29 +0100

    added default cube colors

--- Engine/GeometricTools/ColorTools.h
+++ Engine/GeometricTools/ColorTools.h
@@ -4,4 +4,47 @@ namespace ColorTools{
     constexpr float FullColor[1] = {1.0f};
     constexpr float NoneColor[1] = {0.0f};
     constexpr float Alpha[1] = {1.0f};
+
+        //Cube default colors
+    constexpr float UnitCubeDefaultColor[6*6*3] = { 1.0f, 1.0f, 1.0f,
+                                                    1.0f,1.0f, 1.0f,
+                                                    1.0f,1.0f, 1.0f,
+                                                    1.f,1.f, 1.f,
+                                                    1.f, 1.f, 1.f,
+                                                    1.f, 1.f, 1.f,
+
+                                                    1.f, .0f, .0f,
+                                                    1.f, .0f, .0f,
+                                                    1.f, .0f, .0f,
+                                                    1.f, .0f, .0f,
+                                                    1.f, .0f, .0f,
+                                                    1.f, .0f, .0f,
+
+                                                    .0f, 1.f, .0f,
+                                                    .0f, 1.f, .0f,
+                                                    .0f, 1.f, .0f,
+                                                    .0f, 1.f, .0f,
+                                                    .0f, 1.f, .0f,
+                                                    .0f, 1.f, .0f,
+
+                                                    .0f, .0f, 1.f,
+                                                    .0f, .0f, 1.f,
+                                                    .0f, .0f, 1.f,
+                                                    .0f, .0f, 1.f,
+                                                    .0f, .0f, 1.f,
+                                                    .0f, .0f, 1.f,
+
+                                                    .0f, .0f, .0f,
+                                                    .0f, .0f, .0f,
+                                                    .0f, .0f, .0f,
+                                                    .0f, .0f, .0f,
+                                                    .0f, .0f, .0f,
+                                                    .0f, .0f, .0f,
+
+                                                    .5f,  .5f,.5f,
+                                                    .5f,  .5f,.5f,
+                                                    .5f,  .5f,.5f,
+                                                    .5f,  .5f,.5f,
+                                                    .5f,  .5f,.5f,
+                                                    .5f,  .5f,.5f};
 }
