ede95962350ac8c2c7293d173d958b1e0250d8fa
Robin Ruud Kristensen <robin1998@hotmail.no>
2022-11-03 23:40:05 +0100

    added function for grid with texture coords

--- Engine/GeometricTools/GeometricTools.h
+++ Engine/GeometricTools/GeometricTools.h
@@ -116,9 +116,46 @@ namespace GeometricTools{
                 float y = (float)j/(float)N;
                 //float z = sin(x*2.0f*3.141526f) * sin(y*2.0f*3.141526f) * 0.1f;
                 float z = 0;
+                float t_x = (float)i/(float)N;
+                float t_y = (float)j/(float)N;
                 verticies.push_back(glm::vec3(x,y,z));
+                //verticies.push_back(glm::vec4(x,y,t_x,t_y));
+            }
+        }
+
+        for (int j = 0; j < N; j++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                int row1 = j * (N+1);
+                int row2 = (j+1) * (N+1);
+
+                //Triangle 1
+                indicies.push_back(glm::uvec3(row1 + i,row1 + i + 1,row2 + i + 1));
+
+                //Triangle 2
+                indicies.push_back(glm::uvec3(row1 + i, row2 + i + 1, row2 + i));
             }
             
+        }
+    }
+
+        //https://stackoverflow.com/questions/58494179/how-to-create-a-grid-in-opengl-and-drawing-it-with-lines
+    template<class T, class U> void GenGridTexture(int N, T &verticies, U &indicies){
+        int half_N = N/2;
+        for (float j = 0; j <= N; j++)
+        {
+            for (float i = 0; i <= N; i++)
+            {
+                float x = (float)i/(float)N;
+                float y = (float)j/(float)N;
+                //float z = sin(x*2.0f*3.141526f) * sin(y*2.0f*3.141526f) * 0.1f;
+                float z = 0;
+                float t_x = (float)i/(float)N;
+                float t_y = (float)j/(float)N;
+                verticies.push_back(glm::vec4(x,y,t_x,t_y));
+                //verticies.push_back(glm::vec4(x,y,t_x,t_y));
+            }
         }
 
         for (int j = 0; j < N; j++)
